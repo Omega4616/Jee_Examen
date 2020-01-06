4fc09607c6059859e1221fb08d695d475280388e
LagneauJonathan <lagneaujonathan@gmail.com>
2020-01-06 23:52:20 +0100

    Update Test.java
    testerdu rest

--- src/be/Lagneau/test/Test.java
+++ src/be/Lagneau/test/Test.java
@@ -1,7 +1,7 @@
 package be.Lagneau.test;
 
-import be.BiscontiLagneau.JavaBean.CMedecin;
-import be.BiscontiLagneau.Servlet.AuthentificationMedecin;
+import be.BiscontiLagneau.JavaBean.*;
+import java.util.*;
 
 public class Test {
 
@@ -11,6 +11,13 @@ public class Test {
 		m = m.authentification(14689278354L, "test");
 		
 		System.out.println(m.toString());
+		
+		CMedicament cMedicament = new CMedicament();
+		List<CMedicament> cMedicaments = cMedicament.recupererTousMedicaments();
+		
+		for (CMedicament medoc : cMedicaments) {
+			System.out.println("Nom médoc :" + medoc.getNom() + "Type : " + medoc.getType() + "Dosage : " + medoc.getDosage_jour_max() +  "Description : " + medoc.getDescription() + "ID : "+ medoc.getID_Medicament());
+		}
 
 	}
 
