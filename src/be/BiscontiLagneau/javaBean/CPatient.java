4ac3162820218f06349401cbf4659a29d65d86bb
LagneauJonathan <lagneaujonathan@gmail.com>
2020-01-08 21:36:24 +0100

    Update CPatient.java

--- src/be/BiscontiLagneau/javaBean/CPatient.java
+++ src/be/BiscontiLagneau/javaBean/CPatient.java
@@ -1,24 +1,39 @@
 package be.BiscontiLagneau.JavaBean;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class CPatient extends CPersonne implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	//Attributs
-	int nss; //N° sécurité sociale
+	int ID_Patient;
+	long nrn; //N° de registre national
 	
 	//Constructeurs
 	public CPatient() {}
 	
 	//Accesseurs
-	public int getNss()
-	{
-		return nss;
+	public int getID_Patient() {
+		return ID_Patient;
+	}
+
+	public void setID_Patient(int iD_Patient) {
+		ID_Patient = iD_Patient;
 	}
 	
-	public void setNss(int nss)
+	public long getNrn()
 	{
-		this.nss = nss;
+		return nrn;
+	}
+	
+	public void setNrn(long nrn)
+	{
+		this.nrn = nrn;
+	}
+
+	public List<CPatient> recupererTousMedicaments() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
